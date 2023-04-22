845db08ee3a932a08862d06da3b4548cc425dbde
BFG-KO <83630621+BFGKO@users.noreply.github.com>
2023-04-22 18:07:21 +0300

    added event manager and its functions

---
+++ Modules/EventManager.lua
@@ -0,0 +1,48 @@
+local Manager = {
+    events = {
+
+    }
+}
+
+function Manager:AddEvent(name, event, callback)
+    if self.events[name] then
+        warn("Already have "..name.." registered to events")
+        return
+    end
+
+    self.events[name] = {
+        ["event"] = event,
+        ["function"] = event:Connect(callback)
+    }
+    return true
+end
+
+function Manager:ChangeCallback(name : string, callback)
+    if self.events[name] then
+        warn(name, "doesn't exists")
+        return
+    end
+
+    self.events[name]["function"]:Disconnect()
+    self.events[name]["function"] = self.events[name]["event"]:Connect(callback)
+    return true
+end
+
+function Manager:StopEvent(name : string)
+    if self.events[name] then
+        warn(name, "doesn't exists")
+        return
+    end
+
+    self.events[name]["function"]:Disconnect()
+    self.events[name] = nil
+    return true
+end
+
+function Manager:StopAll()
+    for i,event in pairs(self.events) do
+        self:StopEvent(i)
+    end
+end
+
+return Manager
